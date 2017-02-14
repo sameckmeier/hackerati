3f92f4ee8f7917c133c5f3ba8916a2103fe05fc9
Sam Eckmeier <eckmeier41@gmail.com>
2017-02-14 16:23:18 -0500

    adds store types

---
+++ src/store/types.js
@@ -0,0 +1,13 @@
+import { AUCTIONS } from './auctions/schema';
+import { BIDS } from './bids/schema';
+import { ITEMS } from './items/schema';
+import { USERS } from './users/schema';
+
+const TYPES = [
+  AUCTIONS,
+  BIDS,
+  ITEMS,
+  USERS,
+];
+
+export default TYPES;
