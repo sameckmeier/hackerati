5c1059ce2e4d351d904b15125554144f57303193
Sam Eckmeier <eckmeier41@gmail.com>
2017-02-15 10:52:28 -0500

    adds auctions test scenario that covers when an auction is closed and the highest bid is less than the reserved price of the item

--- src/store/auctions/actions.test.js
+++ src/store/auctions/actions.test.js
@@ -96,10 +96,15 @@ describe('Auctions actions', () => {
       reservedPrice: 1.00,
       sold: false,
     };
-    const bidParams = {
+    const successfulBidParams = {
       participantsId: 1,
       price: 2.00,
     };
+    const unsuccessfulBidParams = {
+      participantsId: 1,
+      price: 0.50,
+    };
+
 
     const buildAuction = () => persist(itemsIdIndexKey(), ITEMS, itemParams)
     .then(itemsId => Bluebird.all([
@@ -130,76 +135,94 @@ describe('Auctions actions', () => {
       return closeAuction(auctionsId, itemsId);
     };
 
+    const buildBid = (bidParams, ids) => {
+      const auctionsId = ids[1];
+      const itemsId = ids[2];
+
+      return (
+        Bluebird.all([
+          auctionsId,
+          itemsId,
+          persist(
+            bidsIdIndexKey(),
+            BIDS,
+            Object.assign({}, bidParams, { auctionsId }),
+          ),
+        ])
+        .then(ids => {
+          const auctionsId = ids[0];
+          const itemsId = ids[1];
+          const bidsId = ids[2];
+
+          return (
+            Bluebird.all([
+              client.rpushAsync(auctionsBidsIdsKey(auctionsId), bidsId),
+              auctionsId,
+              itemsId,
+            ])
+          );
+        })
+      );
+    };
+
+    const updatedValues = ids => {
+      const auctionsId = ids[0];
+      const itemsId = ids[1];
+
+      return (
+        Bluebird.all([
+          get(itemsIdKey(itemsId)),
+          get(auctionsIdKey(auctionsId)),
+        ])
+        .then(res => {
+          const { sold } = res[0];
+          const {
+            winnersId,
+            success,
+            active,
+           } = res[1];
+
+          return {
+            winnersId,
+            sold,
+            success,
+            active,
+          };
+        })
+      );
+    };
+
     describe('when the highest bid is higher than the reserved price of the item', () => {
       after(() => client.flushdb());
-
-      const buildBid = ids => {
-        const auctionsId = ids[1];
-        const itemsId = ids[2];
-
-        return (
-          Bluebird.all([
-            auctionsId,
-            itemsId,
-            persist(
-              bidsIdIndexKey(),
-              BIDS,
-              Object.assign({}, bidParams, { auctionsId }),
-            ),
-          ])
-          .then(ids => {
-            const auctionsId = ids[0];
-            const itemsId = ids[1];
-            const bidsId = ids[2];
-
-            return (
-              Bluebird.all([
-                client.rpushAsync(auctionsBidsIdsKey(auctionsId), bidsId),
-                auctionsId,
-                itemsId,
-              ])
-            );
-          })
-        );
-      };
-
-      const updatedValues = ids => {
-        const auctionsId = ids[0];
-        const itemsId = ids[1];
-
-        return (
-          Bluebird.all([
-            get(itemsIdKey(itemsId)),
-            get(auctionsIdKey(auctionsId)),
-          ])
-          .then(res => {
-            const { sold } = res[0];
-            const {
-              winnersId,
-              success,
-              active,
-             } = res[1];
-
-            return {
-              winnersId,
-              sold,
-              success,
-              active,
-            };
-          })
-        );
-      };
 
       it('should be a success',
         () => chai.expect(
           buildAuction()
-          .then(res => buildBid(res))
+          .then(res => buildBid(successfulBidParams, res))
           .then(res => runCloseAuction(res))
           .then(res => updatedValues(res)),
         ).to.eventually.have.properties({
-          winnersId: bidParams.participantsId,
+          winnersId: successfulBidParams.participantsId,
           sold: true,
           success: true,
+          active: false,
+        }),
+      );
+    });
+
+    describe('when the highest bid is less than the reserved price of the item', () => {
+      after(() => client.flushdb());
+
+      it('should not be a success',
+        () => chai.expect(
+          buildAuction()
+          .then(res => buildBid(unsuccessfulBidParams, res))
+          .then(res => runCloseAuction(res))
+          .then(res => updatedValues(res)),
+        ).to.eventually.have.properties({
+          winnersId: unsuccessfulBidParams.participantsId,
+          sold: true,
+          success: false,
           active: false,
         }),
       );
